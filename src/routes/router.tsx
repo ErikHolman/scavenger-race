a3849e0e7cb4e79ab40c580347198689ea047482
ErikHolman <lifeinthedayof@gmail.com>
2024-03-22 09:51:18 -0700

    scaffold remaining routes

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -15,9 +15,23 @@ export const router = createBrowserRouter([
     path: '/welcome/',
     element: <Welcome />,
   },
-  { path: `/race/:raceId/`, element: <Race /> },
+  // Race Overview route
+  { path: `/race/`, element: <Race /> },
+  // User routes
   { path: '/user/list', element: <UserView /> },
-  { path: `/user/edit/:userId`, element: <div>{`Edit User {:userID}`}</div> },
-  { path: `/user/kill/:userId`, element: <div>{`Kill User {:userID}`}</div> },
-  { path: `/user/new/`, element: <div>New User Page</div> },
+  { path: `/user/edit/:userId`, element: <div>Edit user page</div> },
+  { path: `/user/kill/:userId`, element: <div>Kill user page</div> },
+  { path: `/user/new/`, element: <div>New user page</div> },
+  // Task routes
+  { path: `/task/list/`, element: <div>All tasks page</div> },
+  { path: `/task/edit/:taskId`, element: <div>Edit task page</div> },
+  { path: `/task/kill/:taskId`, element: <div>Kill task page</div> },
+  { path: `/task/new/`, element: <div>New task page</div> },
+  // Leg routes
+  { path: `/leg/list/`, element: <div>All legs page</div> },
+  { path: `/leg/edit/:legId`, element: <div>Edit leg page</div> },
+  { path: `/leg/kill/:legId`, element: <div>Kill leg page</div> },
+  { path: `/leg/new/`, element: <div>New leg page</div> },
+  // Role route
+  { path: `/role/list/`, element: <div>All roles page</div> },
 ]);
