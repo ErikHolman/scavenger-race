40010bc93f024f010fb5d24bf6430a454b8f1f50
ErikHolman <lifeinthedayof@gmail.com>
2024-03-22 09:57:10 -0700

    router updates

--- src/components/tasks/task.tsx
+++ src/components/tasks/task.tsx
@@ -3,17 +3,18 @@ import { Detour } from './detour';
 import { Fast } from './fast';
 import { RoadBlock } from './roadBlock';
 import { RouteInfo } from './routeInfo';
+import { Link } from 'react-router-dom';
 
 type Props = { taskType: string; taskData: { taskId: string } };
 
 export const Task = (props: Props) => {
   return (
-    <div>
+    <Link className='task-edit-link' to={`/task/edit/${props.taskData.taskId}`}>
       <div className='task-id'>Task #{props.taskData.taskId}</div>
       {props.taskType == 'road' && <RoadBlock data={props.taskData} />}
       {props.taskType == 'detour' && <Detour data={props.taskData} />}
       {props.taskType == 'route' && <RouteInfo data={props.taskData} />}
       {props.taskType == 'fast' && <Fast data={props.taskData} />}
-    </div>
+    </Link>
   );
 };
