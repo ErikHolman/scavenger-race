c84e03d488c2d0d725a94ebd5fa7f247ef5d1a7c
ErikHolman <lifeinthedayof@gmail.com>
2024-03-22 14:28:46 -0700

    update sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -2,6 +2,7 @@ import './sidebar.scss';
 import { useState } from 'react';
 import allUsers from '../_sampleData/sampleUsers.json';
 import raceData from '../_sampleData/sampleRace.json';
+import legData from '../_sampleData/sampleLegs.json';
 import { Link } from 'react-router-dom';
 
 export const Sidebar = () => {
@@ -26,75 +27,89 @@ export const Sidebar = () => {
         <div className='opened-sidebar'>
           {sideBarVisible && (
             <div className='race-sidebar'>
-              <button onClick={closeSidebar}>close</button>
-              <div className='leg-data'>
-                Welcome to {currRace.name}!<br />
-                <br /> This race has {currRace.legs.length} legs and is
-                scheduled to start on {currRace.raceDate} at{' '}
-                {currRace.raceStart}.
+              <div className='top'>
+                <button onClick={closeSidebar}>👈</button>
+                <div className='race-data'>
+                  <div className='race-title'>Welcome to {currRace.name}!</div>
+                  <div className='race-detail'>
+                    <div>
+                      <strong>Legs:</strong> <span>{currRace.legs.length}</span>
+                    </div>
+                    <div>
+                      <strong>Date:</strong> <span>{currRace.raceDate}</span>
+                    </div>
+                    <div>
+                      <strong>Start:</strong> <span>{currRace.raceStart}</span>
+                    </div>
+                  </div>
+                </div>
               </div>
               <div className='race-users'>
-                <div className='label'>participants</div>
-                <div className='label'>
-                  drivers: ({currRace.users.drivers.length})
+                <div className='user-title'>Participants</div>
+                <div className='user-label'>
+                  Drivers: ({currRace.users.drivers.length})
                 </div>
-                <ul>
+                <div className='cloud'>
                   {currRace.users.drivers.map((driver, index) => {
                     return (
-                      <div className='cloud'>
-                        <Link
-                          to={`/user/edit/${driver}`}
-                          className='pill driver'
-                          key={index}
-                        >
-                          {userList[driver].first_name}
-                        </Link>
-                      </div>
+                      <Link
+                        to={`/user/edit/${driver}`}
+                        className='pill driver'
+                        key={index}
+                      >
+                        {userList[driver].first_name}
+                      </Link>
                     );
                   })}
-                </ul>
-                <div className='label'>
-                  racers: ({currRace.users.racers.length})
                 </div>
-                <ul>
+                <div className='user-label'>
+                  Racers: ({currRace.users.racers.length})
+                </div>
+                <div className='cloud'>
                   {currRace.users.racers.map((racer, index) => {
                     return (
-                      <div className='cloud'>
-                        <Link
-                          to={`/user/edit/${racer}`}
-                          className='pill racer'
-                          key={index}
-                        >
-                          {userList[racer].first_name}
-                        </Link>
-                      </div>
+                      <Link
+                        to={`/user/edit/${racer}`}
+                        className='pill racer'
+                        key={index}
+                      >
+                        {userList[racer].first_name}
+                      </Link>
                     );
                   })}
-                </ul>
-                <div className='label'>
-                  judges: ({currRace.users.judges.length})
                 </div>
-                <ul>
+                <div className='user-label'>
+                  Judges: ({currRace.users.judges.length})
+                </div>
+                <div className='cloud'>
                   {currRace.users.judges.map((judge, index) => {
                     return (
-                      <div className='cloud'>
-                        <Link
-                          to={`/user/edit/${judge}`}
-                          className='pill judge'
-                          key={index}
-                        >
-                          {userList[judge].first_name}
-                        </Link>
-                      </div>
+                      <Link
+                        to={`/user/edit/${judge}`}
+                        className='pill judge'
+                        key={index}
+                      >
+                        {userList[judge].first_name}
+                      </Link>
                     );
                   })}
-                </ul>
+                </div>
               </div>
               <div className='race-legs'>
-                Leg List:
-                {currRace.legs.map((leg, index) => {
-                  return <li key={index}>{leg}</li>;
-                })}
+                <div className='leg-title'>Leg List:</div>
+                <div className='cloud'>
+                  {legData.map((leg, index) => {
+                    return (
+                      <Link
+                        to={`/leg/edit/${index + 1}`}
+                        className='pill leg'
+                        key={index}
+                      >
+                        leg {index + 1}
+                      </Link>
+                    );
+                  })}
+                </div>
               </div>
             </div>
           )}
@@ -103,7 +118,7 @@ export const Sidebar = () => {
       {!sideBarVisible && (
         <div className='closed-sidebar'>
           <div className='race-sidebar'>
-            {!sideBarVisible && <button onClick={openSidebar}>open</button>}
+            {!sideBarVisible && <button onClick={openSidebar}>👉</button>}
           </div>
         </div>
       )}
