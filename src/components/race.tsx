c0ba39bd90d70847a83fa322b360b9b9818b33a5
ErikHolman <lifeinthedayof@gmail.com>
2024-03-22 10:58:51 -0700

    created sidebar component

--- src/components/race.tsx
+++ src/components/race.tsx
@@ -1,9 +1,7 @@
-import { useState } from 'react';
 import { Leg } from './leg/leg';
+import { Sidebar } from './sidebar';
 import './race.scss';
-import raceData from '../_sampleData/sampleRace.json';
 import legData from '../_sampleData/sampleLegs.json';
-import allUsers from '../_sampleData/sampleUsers.json';
 
 // This view is the overview of the race components
 
@@ -11,78 +9,9 @@ export const Race = () => {
   // global state
   // none yet
 
-  // local state
-  const [sideBarVisible, setSideBarVisible] = useState(true);
-
-  // local non-state
-  const currRace = raceData[0];
-  // let selectedLeg = 0;
-
-  function closeSidebar() {
-    setSideBarVisible(false);
-  }
-  function openSidebar() {
-    setSideBarVisible(true);
-  }
-
   return (
     <div className='race-overview'>
-      {sideBarVisible && (
-        <div className='opened-sidebar'>
-          {sideBarVisible && (
-            <div className='race-sidebar'>
-              <button onClick={closeSidebar}>close</button>
-              <div className='leg-data'>
-                Welcome to {currRace.name}!<br />
-                <br /> This race has {currRace.legs.length} legs and is
-                scheduled to start on {currRace.raceDate} at{' '}
-                {currRace.raceStart}.
-              </div>
-              <div className='race-users'>
-                <div className='label'>participants</div>
-                <span className='label'>drivers:</span> (
-                {currRace.users.drivers.length})
-                <ul>
-                  {currRace.users.drivers.map((driver, index) => {
-                    return (
-                      <>
-                        <li key={index}>{allUsers[`${driver}`]}</li>
-                      </>
-                    );
-                  })}
-                </ul>
-                <span className='label'>racers:</span> (
-                {currRace.users.racers.length})
-                <ul>
-                  {currRace.users.racers.map((racer, index) => {
-                    return <li key={index}>{racer}</li>;
-                  })}
-                </ul>
-                <span className='label'>judges:</span> (
-                {currRace.users.judges.length})
-                <ul>
-                  {currRace.users.judges.map((judge, index) => {
-                    return <li key={index}>{judge}</li>;
-                  })}
-                </ul>
-              </div>
-              <div className='race-legs'>
-                Leg List:
-                {currRace.legs.map((leg, index) => {
-                  return <li key={index}>{leg}</li>;
-                })}
-              </div>
-            </div>
-          )}
-        </div>
-      )}
-      {!sideBarVisible && (
-        <div className='race-sidebar'>
-          <div className='closed-sidebar'>
-            {!sideBarVisible && <button onClick={openSidebar}>open</button>}
-          </div>
-        </div>
-      )}
+      <Sidebar />
       <div className='race'>
         {legData.map((leg, index) => {
           return <Leg legData={leg} legNumber={index + 1} key={index} />;
---
+++ src/components/sidebar.tsx
@@ -0,0 +1,81 @@
+import './sidebar.scss';
+import { useState } from 'react';
+import allUsers from '../_sampleData/sampleUsers.json';
+import raceData from '../_sampleData/sampleRace.json';
+
+export const Sidebar = () => {
+  // local state
+  const [sideBarVisible, setSideBarVisible] = useState(true);
+
+  // local non-state
+  const currRace = raceData[0];
+  // let selectedLeg = 0;
+
+  function closeSidebar() {
+    setSideBarVisible(false);
+  }
+  function openSidebar() {
+    setSideBarVisible(true);
+  }
+
+  return (
+    <>
+      {sideBarVisible && (
+        <div className='opened-sidebar'>
+          {sideBarVisible && (
+            <div className='race-sidebar'>
+              <button onClick={closeSidebar}>close</button>
+              <div className='leg-data'>
+                Welcome to {currRace.name}!<br />
+                <br /> This race has {currRace.legs.length} legs and is
+                scheduled to start on {currRace.raceDate} at{' '}
+                {currRace.raceStart}.
+              </div>
+              <div className='race-users'>
+                <div className='label'>participants</div>
+                <span className='label'>drivers:</span> (
+                {currRace.users.drivers.length})
+                <ul>
+                  {currRace.users.drivers.map((driver, index) => {
+                    return (
+                      <>
+                        <li key={index}>{allUsers[`${driver}`]}</li>
+                      </>
+                    );
+                  })}
+                </ul>
+                <span className='label'>racers:</span> (
+                {currRace.users.racers.length})
+                <ul>
+                  {currRace.users.racers.map((racer, index) => {
+                    return <li key={index}>{racer}</li>;
+                  })}
+                </ul>
+                <span className='label'>judges:</span> (
+                {currRace.users.judges.length})
+                <ul>
+                  {currRace.users.judges.map((judge, index) => {
+                    return <li key={index}>{judge}</li>;
+                  })}
+                </ul>
+              </div>
+              <div className='race-legs'>
+                Leg List:
+                {currRace.legs.map((leg, index) => {
+                  return <li key={index}>{leg}</li>;
+                })}
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+      {!sideBarVisible && (
+        <div className='race-sidebar'>
+          <div className='closed-sidebar'>
+            {!sideBarVisible && <button onClick={openSidebar}>open</button>}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
