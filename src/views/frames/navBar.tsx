ce3779fb0fee0945bf8c34e8ba17d91d42eb3a33
ErikHolman <lifeinthedayof@gmail.com>
2024-04-11 21:16:49 -0700

    update state and css on navbar

--- src/views/frames/navBar.tsx
+++ src/views/frames/navBar.tsx
@@ -1,79 +1,52 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { Roles } from '../../types/roles';
+import { NavLink } from 'react-router-dom';
 import './navBar.scss';
-// import raceData from '../_sampleData/sampleRace.json';
-
-type Event = {
-  target: {
-    id: string;
-  };
-};
 
 export const NavBar = () => {
-  const activeRoleKey = Object.keys(Roles);
-
   const menuItems = [
     {
       name: 'Admin',
-      children: [],
-      visible: false,
       url: '/race/admin',
-      active: false,
     },
     {
       name: 'Builder',
-      children: [],
-      visible: false,
       url: '/race/builder',
-      active: false,
     },
     {
       name: 'Users',
-      children: [],
-      visible: false,
       url: '/race/users/',
-      active: false,
     },
     {
       name: 'Roles',
-      children: [...activeRoleKey],
-      visible: false,
       url: '/race/roles/',
-      active: false,
     },
     {
       name: 'Legs',
-      children: ['leg 1', 'leg 2'],
-      visible: false,
       url: '/race/legs/',
-      active: false,
     },
     {
       name: 'Tasks',
-      children: [],
-      visible: false,
       url: '/race/tasks/',
-      active: false,
     },
   ];
 
-  const [menuBar, setMenuBar] = useState(menuItems);
-
   return (
     <div className='nav-bar'>
-      <div className='buttons'>
-        {menuBar.map((item, index) => {
+      <div className='buttons-container'>
+        {menuItems.map((item, index) => {
+          console.log(item);
           return (
             <>
-              <Link
-                to={`${menuBar[index].url}`}
-                key={`${item}-button`}
-                className={`menu-item ${item.active ? 'active' : ''}`}
-                id={item.name}
+              <NavLink
+                to={`${menuItems[index].url}`}
+                key={`${item.name.toLowerCase()}-button`}
+                className={({ isActive }) =>
+                  isActive ? 'menu-item active' : 'menu-item'
+                }
+                id={item.name.toLowerCase()}
+                state={index}
               >
                 {item.name}
-              </Link>
+              </NavLink>
             </>
           );
         })}
