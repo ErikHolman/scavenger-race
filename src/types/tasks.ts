9df96ae8a90cef3c29f97ac9531ecc1c4fcf32a7
ErikHolman <lifeinthedayof@gmail.com>
2024-05-14 22:09:16 -0700

    task types added here

--- src/types/tasks.ts
+++ src/types/tasks.ts
@@ -4,3 +4,29 @@ export enum Tasks {
   'road',
   'route',
 }
+
+export type TaskParams = {
+  taskId: string;
+  name: string;
+  type: string;
+  challenge?: string;
+  instruction?: string;
+  icon?: string;
+  intro?: {
+    challenge: string;
+    instruction: string;
+    icon: string;
+  };
+  detourA?: {
+    title: string;
+    challenge: string;
+    instruction: string;
+    icon: string;
+  };
+  detourB?: {
+    title: string;
+    challenge: string;
+    instruction: string;
+    icon: string;
+  };
+};
