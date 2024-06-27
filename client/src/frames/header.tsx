67dafba0b1b005875b16d926682eba1b30e23013
ErikHolman <lifeinthedayof@gmail.com>
2024-06-27 11:53:59 -0700

    added new views

---
+++ client/src/frames/header.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@mui/joy';
+import './header.scss';
+
+export default function Header() {
+  return (
+    <Box className='header'>
+      <div>Header</div>
+    </Box>
+  );
+}
